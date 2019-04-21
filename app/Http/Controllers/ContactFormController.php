7ee28e9b206d92ee5c58ea55436e07e8e9742a24
Roman Sorin <rmaximsorin@gmail.com>
2019-04-21 00:44:49 -0400

    Contact Form crm

---
+++ app/Http/Controllers/ContactFormController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ContactForm;
+use Illuminate\Http\Request;
+
+class ContactFormController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+}
