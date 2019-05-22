8b10ba4c1e564989b97391a0e6f843a41930573a
Roman Sorin <rmaximsorin@gmail.com>
2019-05-22 19:00:40 -0400

    csrf token

--- resources/views/layouts/app_alternative.blade.php
+++ resources/views/layouts/app_alternative.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') — Roman Sorin</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('stylesheets')
