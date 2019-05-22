37dc9137dc1f6a638742f4f7298c28d95a0e78dc
Roman Sorin <rmaximsorin@gmail.com>
2019-05-22 18:58:01 -0400

    work pls

--- resources/views/layouts/app_alternative.blade.php
+++ resources/views/layouts/app_alternative.blade.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('stylesheets')
     <!-- Hotjar Tracking Code for https://www.romansorin.com -->
-    <script>
-        (function(h,o,t,j,a,r){
+<script>
+    (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:1327317,hjsv:6};
         a=o.getElementsByTagName('head')[0];
@@ -18,7 +18,7 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
+</script>
 </head>
 
 <body>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,38 +1,59 @@
 <footer class="footer">
     <div class="container content">
-        <div class="columns is-multiline is-mobile">
-            <div class="column is-one-quarter">
-                <h1 class="is-size-1 has-text-black has-text-weight-bold">Let's create together.</h1>
+        <nav class="level">
+            <div class="level-left">
+                <div class="columns is-multiline is-mobile">
+                    <div class="column">
+                        <h1 class="is-size-1 has-text-black has-text-weight-bold">Let's create together.</h1>
+                    </div>
+                    <div class="column">
+                        <form action="">
+                            <div class="field">
+                                <label class="label">Name</label>
+                                <div class="control">
+                                    <input class="input" type="text" placeholder="Text input">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <label class="label">Email</label>
+                                <div class="control">
+                                    <input class="input" type="email" placeholder="Email input" value="hello@">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <label class="label">Message</label>
+                                <div class="control">
+                                    <textarea class="textarea" placeholder="Textarea"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="field is-grouped">
+                                <div class="control">
+                                    <button class="button is-link">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="column is-one-quarter">
-                <div class="field">
-                    <label class="label">Name</label>
-                    <div class="control">
-                        <input class="input" type="text" placeholder="Text input">
+            <div class="level-right">
+                <div class="columns is-multiline is-mobile">
+                    <div class="column">
+                        <h1 class="is-size-1 has-text-black has-text-weight-bold">Contact</h1>
                     </div>
-                </div>
-
-                <div class="field">
-                    <label class="label">Email</label>
-                    <div class="control">
-                        <input class="input" type="email" placeholder="Email input" value="hello@">
-                    </div>
-                </div>
-
-                <div class="field">
-                    <label class="label">Message</label>
-                    <div class="control">
-                        <textarea class="textarea" placeholder="Textarea"></textarea>
-                    </div>
-                </div>
-
-                <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-link">Submit</button>
+                    <div class="column">
+                        <a class="has-text-black-bis" href="https://github.com/romansorin">github</a>
+                        <a class="has-text-black-bis" href="https://linkedin.com/in/romansorin">linkedin</a>
+                        <a class="has-text-black-bis" href="https://twitter.com/rmsorin">twitter</a>
+                        <a class="has-text-black-bis" href="https://instagram.com/rm.sorin">instagram</a>
                     </div>
                 </div>
             </div>
-        </div>
+        </nav>
+
+
 
     </div>
 </footer>
