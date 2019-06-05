a20b5fe80a8770e0eb379ad0e447ce01b36d3136
Roman Sorin <rmaximsorin@gmail.com>
2019-06-05 18:54:40 -0400

    fix: navbar dropdown not working correctly

--- public/js/app.js
+++ public/js/app.js
@@ -54803,7 +54803,24 @@ feather.replace(); // Hotjar Tracking Code for https://www.romansorin.com
   r.async = 1;
   r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
   a.appendChild(r);
-})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv='); // Get all "navbar-burger" elements
+
+
+var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.hamburger'), 0); // Check if there are any navbar burgers
+
+if ($navbarBurgers.length > 0) {
+  // Add a click event on each of them
+  $navbarBurgers.forEach(function (el) {
+    el.addEventListener('click', function () {
+      // Get the target from the "data-target" attribute
+      var target = el.dataset.target;
+      var $target = document.getElementById(target); // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+    });
+  });
+}
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -43,3 +43,25 @@ feather.replace();
     r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
     a.appendChild(r);
 })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+
+// Get all "navbar-burger" elements
+const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.hamburger'), 0);
+
+// Check if there are any navbar burgers
+if ($navbarBurgers.length > 0) {
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+}
