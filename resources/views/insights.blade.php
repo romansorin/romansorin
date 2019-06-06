3d645505bfe0a0cdc967cb20722d779beabc2534
Roman Sorin <rmaximsorin@gmail.com>
2019-06-06 16:44:11 -0400

    insight update

--- resources/views/insights.blade.php
+++ resources/views/insights.blade.php
@@ -7,7 +7,8 @@ Insights from Roman Sorin.
 <section class="hero">
     <div class="hero-body">
         <div class="container hero-wrapper">
-            <h1 class="title">Insights page in progress.</h1>
+            <h1 class="title is-size-1">Insights page in progress.</h1>
+            <h2 class="subtitle is-size-2">Additionally, an insight and overview of developing a dashboard on my portfolio, utilizing the Stripe API for managing invoices, creating ease for clients, and serving as a centralized CRUD for even a simple portfolio. Stay tuned.</h2>
         </div>
     </div>
 </section>
