4601bbd850ef152a8fa07323787c77234e0ec9e0
Roman Sorin <rmaximsorin@gmail.com>
2019-06-06 16:46:52 -0400

    another insight update

--- resources/views/insights.blade.php
+++ resources/views/insights.blade.php
@@ -1,15 +1,27 @@
 @extends('layouts.app')
 @section('title', 'Insights -')
 @section('description')
-Insights from Roman Sorin.
+    Insights from Roman Sorin.
 @endsection
 @section('content')
-<section class="hero">
-    <div class="hero-body">
-        <div class="container hero-wrapper">
-            <h1 class="title is-size-1">Insights page in progress.</h1>
-            <h2 class="subtitle is-size-2">Additionally, an insight and overview of developing a dashboard on my portfolio, utilizing the Stripe API for managing invoices, creating ease for clients, and serving as a centralized CRUD for even a simple portfolio. Stay tuned.</h2>
+    <section class="hero">
+        <div class="hero-body">
+            <div class="container hero-wrapper">
+                <div class="columns">
+                    <div class="column is-two-thirds">
+                        <h1 class="title has-text-weight-bold is-size-1-tablet is-size-2-mobile">Insights page in
+                            progress.</h1>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column is-two-thirds">
+                        <h2 class="subtitle has-text-black-ter is-size-4 is-size-5-mobile">Additionally, an insight and
+                            overview of developing a dashboard on my portfolio, utilizing the Stripe API for managing
+                            invoices, creating ease for clients, and serving as a centralized CRUD for even a simple
+                            portfolio. Stay tuned.</h2>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
