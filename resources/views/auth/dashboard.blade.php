3ef7db17a1e487b04519ed9e756c3fc72b1763da
Roman Sorin <rmaximsorin@gmail.com>
2019-06-06 22:09:46 -0400

    Basic dash & verify views

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+<section class="section">
+    <div class="container">
+        <div class="box">
+            <h1 class="title">Dashboard</h1>
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
+        @endif
+            <h2 class="subtitle">You are logged in!</h2>
         </div>
     </div>
-</div>
+</section>
 @endsection
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -10,12 +10,11 @@
                 {{ __('A fresh verification link has been sent to your email address.') }}
             </div>
             @endif
-            <div class="card-body">
-
+            <h2 class="subtitle">
                 {{ __('Before proceeding, please check your email for a verification link.') }}
                 {{ __('If you did not receive the email') }}, <a
                     href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
-            </div>
+            </h2>
         </div>
     </div>
 </section>
