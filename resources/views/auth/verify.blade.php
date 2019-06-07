8ed48b0eec4d3ea746772920a0455d30f92fdc84
Roman Sorin <rmaximsorin@gmail.com>
2019-06-06 21:50:16 -0400

    Basic verify view

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,22 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<section class="section">
+    <div class="container">
+        <div class="box">
+            <h1 class="title">Verify Your Email Address</h1>
+            @if (session('resent'))
+            <div class="alert alert-success" role="alert">
+                {{ __('A fresh verification link has been sent to your email address.') }}
+            </div>
+            @endif
+            <div class="card-body">
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
-                </div>
+                {{ __('Before proceeding, please check your email for a verification link.') }}
+                {{ __('If you did not receive the email') }}, <a
+                    href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
             </div>
         </div>
     </div>
-</div>
+</section>
 @endsection
