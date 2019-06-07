ef8107bad929455c670f34dee6e5943f27af1d1f
Roman Sorin <rmaximsorin@gmail.com>
2019-06-06 22:03:36 -0400

    misc.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+// FOR AUTH STUFF, USE OUTGOING EMAIL: SECURITY@ROMANSORIN.COM
+
 // dashboard
 ///////////////////
 // Public Routes //
 ///////////////////
-Route::domain('dashboard.romanserver')->group(function() {
+Route::domain('dashboard.romanserver')->group(function () {
     Auth::routes(['verify' => true]);
     Route::get('/', 'DashboardController@index')->name('dashboard');
 });
@@ -13,23 +15,21 @@ Route::domain('dashboard.romanserver')->group(function() {
 ///////////////////
 // Public Routes //
 ///////////////////
-Route::domain('romanserver')->group(function() {
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
-// Route::post('/contact', 'ContactFormController@store');
-Route::get('/insights', function () {
+Route::domain('romanserver')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/about', function () {
+        return view('about');
+    });
+    Route::get('/contact', function () {
+        return view('contact');
+    });
+    // Route::post('/contact', 'ContactFormController@store');
+    Route::get('/insights', function () {
         return view('insights');
-});
-Route::get('/works', function () {
+    });
+    Route::get('/works', function () {
         return view('works');
+    });
 });
-});
-
