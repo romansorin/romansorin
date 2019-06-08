7d4b4f6af0de0944494d5acdd9a89c1ae7d826f3
Roman Sorin <988244@students.mentorschools.org>
2019-06-08 13:19:02 -0400

    Added work views for auth

---
+++ resources/views/auth/works/edit.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.auth')
+@section('title', 'Edit work ...')
+{{-- Should I put truncated name of work title? --}}
+@section('description')
+{{-- Is this necessary, will it even be indexed for SEO? --}}
+@endsection
+@section('content')
+@endsection
