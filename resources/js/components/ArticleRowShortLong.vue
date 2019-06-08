08485a2f7af01306c475ac0fd58be98c432f38b7
Roman Sorin <988244@students.mentorschools.org>
2019-06-08 17:05:21 -0400

    Create vue components for works rows

---
+++ resources/js/components/ArticleRowShortLong.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="container article-container">
+      <div class="tile is-ancestor">
+        <div class="columns is-desktop">
+          <div class="column">
+            <div class="tile is-parent">
+              <article-tile-short v-bind:work="works[0]"/>
+            </div>
+          </div>
+          <div class="column is-offset-1-widescreen">
+            <div class="tile is-parent">
+              <article-tile-long v-bind:work="works[1]"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      works: [
+        {
+          title: "test title one",
+          subtitle: "test subtitle one",
+          image:
+            "https://images.pexels.com/photos/1670187/pexels-photo-1670187.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        },
+        {
+          title: "test title two",
+          subtitle: "test subtitle two",
+          image:
+            "https://images.pexels.com/photos/1670187/pexels-photo-1670187.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        }
+      ]
+    };
+  }
+};
+</script>
