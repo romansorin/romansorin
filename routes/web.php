3301f5a93f88a02988841b491646e91a3e871b46
Roman Sorin <rmaximsorin@gmail.com>
2019-06-08 08:49:56 -0400

    comment

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 $BASE_URL = config('app')['base_url'];
 
 // FOR AUTH STUFF, USE OUTGOING EMAIL: SECURITY@ROMANSORIN.COM
+// FOR SUPPORT, USE OUTGOING EMAIL: SUPPORT@ROMANSORIN.COM
 // Eventually, change the dashboard route of all password / email related things to something different (i.e., not /password/reset or /email/verify). For now, it's functional
 // dashboard
 ///////////////////
