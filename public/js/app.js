6c958888730fd24f2ce6db449a76ce27e3d2a2b2
Roman Sorin <988244@students.mentorschools.org>
2019-06-08 22:54:07 -0400

    Container confusion!

--- public/js/app.js
+++ public/js/app.js
@@ -1856,21 +1856,9 @@ module.exports = function isBuffer (obj) {
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/ArticleContainer.vue?vue&type=script&lang=js& ***!
   \***************************************************************************************************************************************************************************/
 /*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-//
-//
-//
-//
-//
-//
-/* harmony default export */ __webpack_exports__["default"] = ({
-  props: {
-    works: []
-  }
-});
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/988244/Documents/romansorin/resources/js/components/ArticleContainer.vue: Unexpected token, expected \",\" (45:1)\n\n\u001b[0m \u001b[90m 43 | \u001b[39m    }\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 44 | \u001b[39m  }\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 45 | \u001b[39m}\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    | \u001b[39m \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 46 | \u001b[39m\u001b[0m\n    at Parser.raise (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:3851:17)\n    at Parser.unexpected (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:5165:16)\n    at Parser.expect (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:5151:28)\n    at Parser.parseObj (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:6639:14)\n    at Parser.parseExprAtom (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:6276:21)\n    at Parser.parseExprSubscripts (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:5916:23)\n    at Parser.parseMaybeUnary (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:5896:21)\n    at Parser.parseExprOps (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:5783:23)\n    at Parser.parseMaybeConditional (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:5756:23)\n    at Parser.parseMaybeAssign (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:5703:21)\n    at Parser.parseExportDefaultExpression (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:8478:24)\n    at Parser.parseExport (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:8373:31)\n    at Parser.parseStatementContent (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:7397:27)\n    at Parser.parseStatement (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:7293:17)\n    at Parser.parseBlockOrModuleBlockBody (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:7879:25)\n    at Parser.parseBlockBody (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:7866:10)\n    at Parser.parseTopLevel (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:7222:10)\n    at Parser.parse (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:8871:17)\n    at parse (/Users/988244/Documents/romansorin/node_modules/@babel/parser/lib/index.js:11133:38)\n    at parser (/Users/988244/Documents/romansorin/node_modules/@babel/core/lib/transformation/normalize-file.js:170:34)\n    at normalizeFile (/Users/988244/Documents/romansorin/node_modules/@babel/core/lib/transformation/normalize-file.js:138:11)\n    at runSync (/Users/988244/Documents/romansorin/node_modules/@babel/core/lib/transformation/index.js:44:43)\n    at runAsync (/Users/988244/Documents/romansorin/node_modules/@babel/core/lib/transformation/index.js:35:14)\n    at process.nextTick (/Users/988244/Documents/romansorin/node_modules/@babel/core/lib/transform.js:34:34)\n    at processTicksAndRejections (internal/process/next_tick.js:74:9)");
 
 /***/ }),
 
@@ -42543,26 +42531,8 @@ exports.clearImmediate = (typeof self !== "undefined" && self.clearImmediate) ||
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
-var render = function() {
-  var _vm = this
-  var _h = _vm.$createElement
-  var _c = _vm._self._c || _h
-  return _c(
-    "div",
-    [
-      _c("article-row-long-short", {
-        attrs: { work_one: _vm.works[0], work_two: _vm.works[1] }
-      }),
-      _vm._v(" "),
-      _c("article-row-short-long", {
-        attrs: { work_one: _vm.works[2], work_two: _vm.works[3] }
-      })
-    ],
-    1
-  )
-}
+var render = function () {}
 var staticRenderFns = []
-render._withStripped = true
 
 
 
--- resources/js/components/ArticleContainer.vue
+++ resources/js/components/ArticleContainer.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
-    <article-row-long-short v-bind:work_one="works[0]" v-bind:work_two="works[1]"></article-row-long-short>
-    <article-row-short-long v-bind:work_one="works[2]" v-bind:work_two="works[3]"></article-row-short-long>
+    <div v-for="n in works.length" v-bind:key="n">
+      <article-row-long-short
+        v-if="rowLS === true"
+        v-bind:work_one="works[n-1]"
+        v-bind:work_two="works[n]"
+      ></article-row-long-short>
+      {{ rowLS = false }}
+      <article-row-short-long v-else v-bind:work_one="works[n-1]" v-bind:work_two="works[n]"></article-row-short-long>
+      {{ rowLS = true }}
+    </div>
   </div>
 </template>
 <script>
 export default {
   props: {
-    works: []
+    works: {
+      type: Array
+    }
+  },
+  computed: {
+    indicesToSplit: function() {
+      let indices = [];
+      for (let i = 0; i < this.works.length; i++) {
+        if (i % 2 == 0) indices.push(i);
+      }
+      return indices;
+    }
+  },
+  methods: {
+    isIndexEven: function(index) {
+      if (index % 2 == 0) return true;
+    },
+    alternateRow: function(index) {
+      if (this.isIndexEven(index)) this.rowLS = true;
+      else this.rowLS = false;
+      return this.rowLS;
+  },
+  data() {
+    return {
+      rowLS: true
+    };
   }
 };
 </script>
