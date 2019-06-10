ded660a846c046565f8c93a02edbcf6fd6079fb8
Roman Sorin <988244@students.mentorschools.org>
2019-06-10 19:27:35 -0400

    Fixed user class

--- app/User.php
+++ app/User.php
@@ -1,59 +1,4 @@
-
-
-namespace romansorin;
-
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Contracts\Auth\CanResetPassword;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Cashier\Billable;
-use romansorin\Notifications\ResetPassword;
-use romansorin\Notifications\VerifyEmail;
-
-class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
-{
-    use Notifiable;
-    use Billable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    /**
-     * Send the password reset notification.
-     *
-     * @param  string  $token
-     * @return void
-     */
-    public function sendPasswordResetNotification($token)
-    {
-        $this->notify(new ResetPassword($token));
-    }
-}
-
+<?php
 
 namespace romansorin;
 
@@ -113,12 +58,14 @@ class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
      *
      * @return void
      */
-    public function sendEmailVerificationNotification() {
+    public function sendEmailVerificationNotification()
+    {
         $this->notify(new VerifyEmail());
     }
-   
 
-    public function createCustomer() {
+
+    public function createCustomer()
+    {
         $this->createAsStripeCustomer([
             'description' => 'Customer automatically created through registration at romansorin.com',
             'name' => $this->name
