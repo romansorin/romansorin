975c10bba6dd19ae71d95f79cb09fadea4e1efb8
Roman Sorin <rmaximsorin@gmail.com>
2019-07-14 01:13:35 -0400

    Created InsightListComponent and moved <main> content in

---
+++ resources/js/components/InsightsListComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <ul>
+      <div v-for="item in filteredItems">
+        <p>{{item.name}}</p>
+      </div>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  props: ['filteredItems']
+};
+</script>
+
+<style>
+</style>
