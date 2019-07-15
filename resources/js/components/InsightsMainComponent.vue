01e0b824d137d0ed2a064585254da0ec93f14035
Roman Sorin <rmaximsorin@gmail.com>
2019-07-14 23:38:27 -0400

    Added in mobile dropdown into main component, responsiveness

--- resources/js/components/InsightsMainComponent.vue
+++ resources/js/components/InsightsMainComponent.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="columns">
-    <div class="column is-3 is-hidden-mobile">
+  <div class="columns is-multiline">
+    <div class="column is-3 is-hidden-touch">
       <InsightsSidebarComponent v-on:categoryChanged="setCategory($event)" />
+    </div>
+    <div class="column is-hidden-desktop is-12">
+      <InsightsMobileComponent v-on:categoryChanged="setCategory($event)" />
     </div>
     <div class="column">
       <main class="insight-articles-wrapper">
@@ -19,16 +22,37 @@
 <script>
 import InsightsSidebarComponent from "./InsightsSidebarComponent";
 import InsightsMainComponent from "./InsightsMainComponent";
+import InsightsMobileComponent from "./InsightsMobileComponent";
 
 export default {
   data() {
     return {
       selectedCategory: "",
       items: [
-        { title: "Lorem ipsum development mething", category: "Development", date_published: Date(), link: "" },
-        { title: "Dolor design blah blah", category: "Design", date_published: Date(), link: "" },
-        { title: "For whatever reason, personal life on a portfolio", category: "Personal", date_published: Date(), link: "" },
-        { title: "Something something wantepreneur?", category: "Business", date_published: Date(), link: "" }
+        {
+          title: "Lorem ipsum development mething",
+          category: "Development",
+          date_published: Date(),
+          link: ""
+        },
+        {
+          title: "Dolor design blah blah",
+          category: "Design",
+          date_published: Date(),
+          link: ""
+        },
+        {
+          title: "For whatever reason, personal life on a portfolio",
+          category: "Personal",
+          date_published: Date(),
+          link: ""
+        },
+        {
+          title: "Something something wantepreneur?",
+          category: "Business",
+          date_published: Date(),
+          link: ""
+        }
       ]
     };
   },
@@ -42,11 +66,13 @@ export default {
       if (this.selectedCategory == "") return this.items;
       else
         return this.items.filter(item => {
-          return item.category.toLowerCase() == this.selectedCategory.toLowerCase();
+          return (
+            item.category.toLowerCase() == this.selectedCategory.toLowerCase()
+          );
         });
     }
   },
-  components: { InsightsSidebarComponent, InsightsMainComponent }
+  components: { InsightsSidebarComponent, InsightsMainComponent, InsightsMobileComponent }
 };
 </script>
 
